58eb093a7a46e5ff04c708f5ea13b292897777d6
wangpiece <wangpiece.xu@163.com>
2018-09-23 19:22:48 +0800

    nexus仓库配置

---
+++ src/main/java/com/wangpiece/controller/IdeaTest.java
@@ -0,0 +1,15 @@
+package com.wangpiece.controller;
+
+public class IdeaTest {
+
+    public static void main(String[] args) {
+        Integer abc = 1;
+        Integer abc11 = 1;
+        Integer abc2 = 1;
+        Integer abc3 = 1;
+
+        if(abc > 3) abc3 = 5;
+
+        System.out.println(abc3);
+    }
+}
